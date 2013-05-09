c63add590afa98d3da3be796c37385081cde98e4
spprof <spprof@yandex.ru>
2013-05-09 22:57:06 +0400

    -

--- themes/default/views/site/index.php
+++
@@ -1,7 +0,0 @@
-<?php $this->pageTitle = $this->yupe->siteName; ?>
-
-<?php $this->widget('zii.widgets.CListView', array(
-    'dataProvider' => $dataProvider,
-    'itemView'     => '_view',
-    'template'     => "{items}\n{pager}",
-)); ?>
